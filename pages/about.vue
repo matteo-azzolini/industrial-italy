09d8220b8582c2406494cf34ff7e66008ff29599
dakko <matteo.azzolini@eng.it>
2023-03-31 19:57:35 +0200

    About

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@ definePageMeta({
 
 <template>
   <div
-    class="prose prose-invert pt-20 sm:max-w-2xl max-w-5xl
+    class="prose prose-invert pt-12 md:pt-20 sm:max-w-2xl max-w-5xl
       mx-auto px-6 md:px-0"
   >
     <h1>I like industrial buildings</h1>
